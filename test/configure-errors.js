1ff45b49ffa7b92c6aad462bfefe692ed174e45c
Bert Casier <bert@ernie.be>
2016-04-26 13:29:41 +0200

    refactor tests
    update feedback from
    https://github.com/strongloop/loopback-component-passport/pull/142#issue
    comment-214442257

--- test/configure-errors.js
+++ test/configure-errors.js
@@ -1,107 +1,108 @@
-(function() {
-  var loopback = require('loopback');
-  var app = loopback();
-  var m = require('./init');
-  var mock = require('mock-require');
-  var PassportConfigurator = require('../lib/passport-configurator.js');
-  var passportConfigurator = new PassportConfigurator(app);
-  var passport = require('passport');
-  var assert = require('assert');
-  var request = require('supertest');
-  var sinon = require('sinon');
+
+var loopback = require('loopback');
+var app = loopback();
+var m = require('./init');
+var mock = require('mock-require');
+var PassportConfigurator = require('../lib/passport-configurator.js');
+var passport = require('passport');
+var assert = require('assert');
+var request = require('supertest');
+var sinon = require('sinon');
+
+describe('Login failure', function() {
   var passportAuthenticate;
 
-  //mock a strategy to work with
-  function PassportFacebookStrategy(options, verify) {};
-  PassportFacebookStrategy.prototype.authenticate = function(req, options) {};
-  mock('passport-facebook', PassportFacebookStrategy);
+  before('Configure facebook strategy', function() {
+    var passportConfigurator = new PassportConfigurator(app);
 
-  describe('Login failure', function() {
-    before(function(done) {
-      var options = {
-        'provider': 'facebook',
-        'module': 'passport-facebook',
-        'callbackURL': '/auth/facebook/callback',
-        'authPath': '/auth/facebook',
-        'callbackPath': '/auth/facebook/callback',
-        'successRedirect': '/auth/facebook/redirect',
-        'failureRedirect': '/auth/facebook-failure',
-        'failureQueryString': true,
-        'link': false,
-      };
-      passportConfigurator.configureProvider('facebook-login', options);
-      done();
-    });
+    //mock a strategy to work with
+    function PassportFacebookStrategy(options, verify) {};
+    PassportFacebookStrategy.prototype.authenticate = function(req, options) {};
+    mock('passport-facebook', PassportFacebookStrategy);
 
-    afterEach(function(done) {
-      if (passportAuthenticate) passportAuthenticate.restore();
-      done();
-    });
-
-    it('redirects to failure with error', function(done) {
-      passportAuthenticate = sinon.stub(passport, 'authenticate',
-          getAuthenticateStubFunction('horrible mistake'));
-      request(app)
-          .get('/auth/facebook/callback')
-          .expect(302)
-          .end(function(err, response) {
-            assert.equal(response.headers.location, '/auth/facebook-failure?error=horrible%20mistake');
-            done();
-          });
-    });
-
-    it('redirects to failure without error if empty string', function(done) {
-      passportAuthenticate = sinon.stub(passport, 'authenticate', getAuthenticateStubFunction(''));
-      request(app)
-          .get('/auth/facebook/callback')
-          .expect(302)
-          .end(function(err, response) {
-            assert.equal(response.headers.location, '/auth/facebook-failure');
-            done();
-          });
-    });
+    var options = {
+      'provider': 'facebook',
+      'module': 'passport-facebook',
+      'callbackURL': '/auth/facebook/callback',
+      'authPath': '/auth/facebook',
+      'callbackPath': '/auth/facebook/callback',
+      'successRedirect': '/auth/facebook/redirect',
+      'failureRedirect': '/auth/facebook-failure',
+      'failureQueryString': true,
+      'link': false,
+    };
+    passportConfigurator.configureProvider('facebook-login', options);
   });
 
-  describe('Link failure', function() {
-    before(function(done) {
-      var linkOptions = {
-        'provider': 'facebook',
-        'module': 'passport-facebook',
-        'callbackURL': '/auth/facebook-link/callback',
-        'authPath': '/auth/facebook-link',
-        'callbackPath': '/auth/facebook-link/callback',
-        'successRedirect': '/auth/facebook-link/redirect',
-        'failureRedirect': '/auth/facebook-failure',
-        'failureQueryString': true,
-        'link': true,
-      };
-      var passportAuthorize = sinon.stub(passport, 'authorize', getAuthorizeStubFunction('omg'));
-      passportConfigurator.configureProvider('facebook-link', linkOptions);
-      passportAuthorize.restore();
-      done();
-    });
-
-    it('redirects to failure with error', function(done) {
-      request(app)
-          .get('/auth/facebook-link/callback')
-          .expect(302)
-          .end(function(err, response) {
-            assert.equal(response.headers.location, '/auth/facebook-failure?error=omg');
-            done();
-          });
-    });
+  afterEach('Cleanup mocks', function() {
+    if (passportAuthenticate) passportAuthenticate.restore();
   });
 
-  function getAuthenticateStubFunction(wantedError) {
-    return function(name, options, cb) {
-      if (cb) cb(null, false, wantedError);
-      return function(req, res, next) { next(); };
-    };
-  }
+  it('redirects to failure with error', function(done) {
+    passportAuthenticate = sinon.stub(passport, 'authenticate',
+      getAuthenticateStubFunction('horrible mistake'));
+    request(app)
+      .get('/auth/facebook/callback')
+      .expect(302)
+      .end(function(err, res) {
+        assert.equal(res.headers.location, '/auth/facebook-failure?error=horrible%20mistake');
+        done();
+      });
+  });
 
-  function getAuthorizeStubFunction(err) {
-    return function(passport, name, options, callback) {
-      return function(req, res, next) { next(err); };
+  it('redirects to failure without error if empty string', function(done) {
+    passportAuthenticate = sinon.stub(passport, 'authenticate', getAuthenticateStubFunction(''));
+    request(app)
+      .get('/auth/facebook/callback')
+      .expect(302)
+      .end(function(err, res) {
+        assert.equal(res.headers.location, '/auth/facebook-failure');
+        done();
+      });
+  });
+});
+
+describe('Link failure', function() {
+  before('Setup facebook strategy', function() {
+    var passportConfigurator = new PassportConfigurator(app);
+    var linkOptions = {
+      'provider': 'facebook',
+      'module': 'passport-facebook',
+      'callbackURL': '/auth/facebook-link/callback',
+      'authPath': '/auth/facebook-link',
+      'callbackPath': '/auth/facebook-link/callback',
+      'successRedirect': '/auth/facebook-link/redirect',
+      'failureRedirect': '/auth/facebook-failure',
+      'failureQueryString': true,
+      'link': true,
     };
-  }
-})();
+
+    //passportConfigurator.configureProvider internally uses passport.authorize so stub it
+    var passportAuthorize = sinon.stub(passport, 'authorize', getAuthorizeStubFunction('omg'));
+    passportConfigurator.configureProvider('facebook-link', linkOptions);
+    passportAuthorize.restore();
+  });
+
+  it('redirects to failure with error', function(done) {
+    request(app)
+      .get('/auth/facebook-link/callback')
+      .expect(302)
+      .end(function(err, res) {
+        assert.equal(res.headers.location, '/auth/facebook-failure?error=omg');
+        done();
+      });
+  });
+});
+
+function getAuthenticateStubFunction(wantedError) {
+  return function(name, options, cb) {
+    if (cb) cb(null, false, wantedError);
+    return function(req, res, next) { next(); };
+  };
+}
+
+function getAuthorizeStubFunction(err) {
+  return function(passport, name, options, callback) {
+    return function(req, res, next) { next(err); };
+  };
+}
