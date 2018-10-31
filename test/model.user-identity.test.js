65a10cf998881d0fb7bc5ba3a698d77fc8357a13
Natthanun Chantanurak <natthanun@skooldio.com>
2018-10-31 11:32:15 +0700

    fix for different provider but same externalId

--- test/model.user-identity.test.js
+++ test/model.user-identity.test.js
@@ -22,11 +22,15 @@ describe('UserIdentity', function() {
   });
 
   it('supports 3rd party login', function(done) {
-    UserIdentity.login('facebook', 'oAuth 2.0',
-      {emails: [
-        {value: 'foo@bar.com'},
-      ], id: 'f123', username: 'xyz',
-      }, {accessToken: 'at1', refreshToken: 'rt1'},
+    UserIdentity.login(
+      'facebook',
+      'oAuth 2.0',
+      {
+        emails: [{value: 'foo@bar.com'}],
+        id: 'f123',
+        username: 'xyz',
+      },
+      {accessToken: 'at1', refreshToken: 'rt1'},
       {autoLogin: false},
       function(err, user, identity, token) {
         assert(!err, 'No error should be reported');
@@ -48,21 +52,23 @@ describe('UserIdentity', function() {
           assert.equal(user.email, 'xyz@loopback.facebook.com');
           done();
         });
-      });
+      }
+    );
   });
 
   it('supports 3rd party login with custom field mapping', function(done) {
-    UserIdentity.login('linkedin', 'oAuth 2.0',
+    UserIdentity.login(
+      'linkedin',
+      'oAuth 2.0',
       {
-        emails: [
-          {value: 'johndoe@foobar.com'},
-        ],
+        emails: [{value: 'johndoe@foobar.com'}],
         id: 'f456',
         name: {
           givenName: 'John',
           familyName: 'Doe',
         },
-      }, {accessToken: 'at1', refreshToken: 'rt1'},
+      },
+      {accessToken: 'at1', refreshToken: 'rt1'},
       {
         autoLogin: false,
         profileMapping: [
@@ -99,100 +105,185 @@ describe('UserIdentity', function() {
           assert.equal(user.lastName, 'Doe');
           done();
         });
-      });
+      }
+    );
   });
 
   it('supports 3rd party login if the identity already exists', function(done) {
-    User.create({
-      username: 'facebook.abc',
-      email: 'abc@facebook.com',
-      password: 'pass',
-    }, function(err, user) {
-      if (err) return done(err);
-      UserIdentity.create({
-        externalId: 'f456',
-        provider: 'facebook',
-        userId: user.id,
-        authScheme: 'oAuth 2.0',
-      }, function(err, identity) {
+    User.create(
+      {
+        username: 'facebook.abc',
+        email: 'abc@facebook.com',
+        password: 'pass',
+      },
+      function(err, user) {
         if (err) return done(err);
-        UserIdentity.login('facebook', 'oAuth 2.0',
-          {emails: [
-            {value: 'abc1@facebook.com'},
-          ], id: 'f456', username: 'xyz',
-          }, {accessToken: 'at2', refreshToken: 'rt2'}, function(err, user, identity, token) {
+        UserIdentity.create(
+          {
+            externalId: 'f456',
+            provider: 'facebook',
+            userId: user.id,
+            authScheme: 'oAuth 2.0',
+          },
+          function(err, identity) {
             if (err) return done(err);
-            assert.equal(user.username, 'facebook.abc');
-            assert.equal(user.email, 'abc@facebook.com');
+            UserIdentity.login(
+              'facebook',
+              'oAuth 2.0',
+              {
+                emails: [{value: 'abc1@facebook.com'}],
+                id: 'f456',
+                username: 'xyz',
+              },
+              {accessToken: 'at2', refreshToken: 'rt2'},
+              function(err, user, identity, token) {
+                if (err) return done(err);
+                assert.equal(user.username, 'facebook.abc');
+                assert.equal(user.email, 'abc@facebook.com');
 
-            assert.equal(identity.externalId, 'f456');
+                assert.equal(identity.externalId, 'f456');
+                assert.equal(identity.provider, 'facebook');
+                assert.equal(identity.authScheme, 'oAuth 2.0');
+                assert.deepEqual(identity.credentials, {accessToken: 'at2', refreshToken: 'rt2'});
+
+                assert.equal(user.id, identity.userId);
+
+                assert(token);
+
+                // Follow the belongsTo relation
+                identity.user(function(err, user) {
+                  assert(!err, 'No error should be reported');
+                  assert.equal(user.username, 'facebook.abc');
+                  assert.equal(user.email, 'abc@facebook.com');
+                  done();
+                });
+              }
+            );
+          }
+        );
+      }
+    );
+  });
+
+  it('supports 3rd party login with different providers but same externalId', function(done) {
+    User.create(
+      {
+        username: 'facebook.abc',
+        email: 'abc@facebook.com',
+        password: 'pass',
+      },
+      function(err, user) {
+        if (err) return done(err);
+        UserIdentity.create(
+          {
+            externalId: 'f456',
+            provider: 'facebook',
+            userId: user.id,
+            authScheme: 'oAuth 2.0',
+          },
+          function(err, identity) {
+            if (err) return done(err);
+            UserIdentity.login(
+              'new-provider',
+              'oAuth 2.0',
+              {
+                emails: [{value: 'xyz@newprovider.com'}],
+                id: 'f456',
+                username: 'xyz',
+              },
+              {accessToken: 'at2', refreshToken: 'rt2'},
+              function(err, user, identity, token) {
+                if (err) return done(err);
+                assert.equal(user.username, 'new-provider.xyz');
+                assert.equal(user.email, 'xyz@loopback.new-provider.com');
+
+                assert.equal(identity.externalId, 'f456');
+                assert.equal(identity.provider, 'new-provider');
+                assert.equal(identity.authScheme, 'oAuth 2.0');
+                assert.deepEqual(identity.credentials, {accessToken: 'at2', refreshToken: 'rt2'});
+
+                assert.equal(user.id, identity.userId);
+
+                assert(token);
+
+                // Follow the belongsTo relation
+                identity.user(function(err, user) {
+                  assert(!err, 'No error should be reported');
+                  assert.equal(user.username, 'new-provider.xyz');
+                  assert.equal(user.email, 'xyz@loopback.new-provider.com');
+                  done();
+                });
+              }
+            );
+          }
+        );
+      }
+    );
+  });
+
+  it('supports 3rd party login if user account already exists', function(done) {
+    User.create(
+      {
+        username: 'facebook.789',
+        email: '789@facebook.com',
+        password: 'pass',
+      },
+      function(err, user) {
+        UserIdentity.login(
+          'facebook',
+          'oAuth 2.0',
+          {
+            emails: [{value: '789@facebook.com'}],
+            id: 'f789',
+            username: 'ttt',
+          },
+          {accessToken: 'at3', refreshToken: 'rt3'},
+          function(err, user, identity, token) {
+            assert(!err, 'No error should be reported');
+            assert.equal(user.username, 'facebook.ttt');
+            assert.equal(user.email, 'ttt@loopback.facebook.com');
+
+            assert.equal(identity.externalId, 'f789');
             assert.equal(identity.provider, 'facebook');
             assert.equal(identity.authScheme, 'oAuth 2.0');
-            assert.deepEqual(identity.credentials, {accessToken: 'at2', refreshToken: 'rt2'});
+            assert.deepEqual(identity.credentials, {accessToken: 'at3', refreshToken: 'rt3'});
 
             assert.equal(user.id, identity.userId);
-
             assert(token);
 
             // Follow the belongsTo relation
             identity.user(function(err, user) {
               assert(!err, 'No error should be reported');
-              assert.equal(user.username, 'facebook.abc');
-              assert.equal(user.email, 'abc@facebook.com');
+              assert.equal(user.username, 'facebook.ttt');
+              assert.equal(user.email, 'ttt@loopback.facebook.com');
               done();
             });
-          });
-      });
-    });
-  });
-
-  it('supports 3rd party login if user account already exists', function(done) {
-    User.create({
-      username: 'facebook.789',
-      email: '789@facebook.com',
-      password: 'pass',
-    }, function(err, user) {
-      UserIdentity.login('facebook', 'oAuth 2.0',
-        {emails: [
-          {value: '789@facebook.com'},
-        ], id: 'f789', username: 'ttt',
-        }, {accessToken: 'at3', refreshToken: 'rt3'}, function(err, user, identity, token) {
-          assert(!err, 'No error should be reported');
-          assert.equal(user.username, 'facebook.ttt');
-          assert.equal(user.email, 'ttt@loopback.facebook.com');
-
-          assert.equal(identity.externalId, 'f789');
-          assert.equal(identity.provider, 'facebook');
-          assert.equal(identity.authScheme, 'oAuth 2.0');
-          assert.deepEqual(identity.credentials, {accessToken: 'at3', refreshToken: 'rt3'});
-
-          assert.equal(user.id, identity.userId);
-          assert(token);
-
-          // Follow the belongsTo relation
-          identity.user(function(err, user) {
-            assert(!err, 'No error should be reported');
-            assert.equal(user.username, 'facebook.ttt');
-            assert.equal(user.email, 'ttt@loopback.facebook.com');
-            done();
-          });
-        });
-    });
+          }
+        );
+      }
+    );
   });
 
   it('supports 3rd party login with profileToUser option', function(done) {
-    UserIdentity.login('facebook', 'oAuth 2.0',
-      {emails: [
-        {value: 'foo@baz.com'},
-      ], id: 'f100', username: 'joy',
-      }, {accessToken: 'at1', refreshToken: 'rt1'}, {
+    UserIdentity.login(
+      'facebook',
+      'oAuth 2.0',
+      {
+        emails: [{value: 'foo@baz.com'}],
+        id: 'f100',
+        username: 'joy',
+      },
+      {accessToken: 'at1', refreshToken: 'rt1'},
+      {
         profileToUser: function(provider, profile) {
           return {
             username: profile.username + '@facebook',
             email: profile.emails[0].value,
             password: 'sss',
           };
-        }}, function(err, user, identity, token) {
+        },
+      },
+      function(err, user, identity, token) {
         assert(!err, 'No error should be reported');
         assert.equal(user.username, 'joy@facebook');
         assert.equal(user.email, 'foo@baz.com');
@@ -212,15 +303,20 @@ describe('UserIdentity', function() {
           assert.equal(user.email, 'foo@baz.com');
           done();
         });
-      });
+      }
+    );
   });
 
   it('supports 3rd party login with profileToUser option - manually id set', function(done) {
-    UserIdentity.login('facebook', 'oAuth 2.0',
-      {emails: [
-        {value: 'foo@baz.com'},
-      ], username: 'joy',
-      }, {accessToken: 'at1', refreshToken: 'rt1'}, {
+    UserIdentity.login(
+      'facebook',
+      'oAuth 2.0',
+      {
+        emails: [{value: 'foo@baz.com'}],
+        username: 'joy',
+      },
+      {accessToken: 'at1', refreshToken: 'rt1'},
+      {
         profileToUser: function(provider, profile) {
           profile.id = profile.emails[0].value;
           return {
@@ -228,7 +324,9 @@ describe('UserIdentity', function() {
             email: profile.emails[0].value,
             password: 'sss',
           };
-        }}, function(err, user, identity, token) {
+        },
+      },
+      function(err, user, identity, token) {
         assert(!err, 'No error should be reported');
         assert.equal(user.username, 'joy@facebook');
         assert.equal(user.email, 'foo@baz.com');
@@ -248,129 +346,158 @@ describe('UserIdentity', function() {
           assert.equal(user.email, 'foo@baz.com');
           done();
         });
-      });
+      }
+    );
   });
 
   it('supports ldap login', function(done) {
-    var identity = {emails: [{value: 'fooldap@bar.com'}], id: 'f123ldap',
-      username: 'xyzldap'};
+    var identity = {
+      emails: [{value: 'fooldap@bar.com'}],
+      id: 'f123ldap',
+      username: 'xyzldap',
+    };
     var credentials = {accessToken: 'atldap1', refreshToken: 'rtldap1'};
     var options = {autoLogin: false};
-    UserIdentity.login('ldap', 'ldap', identity, credentials, options,
-      function(err, user, identity, token) {
+    UserIdentity.login('ldap', 'ldap', identity, credentials, options, function(
+      err,
+      user,
+      identity,
+      token
+    ) {
+      if (err) return done(err);
+
+      assert.equal(user.username, 'ldap.xyzldap');
+      assert.equal(user.email, 'fooldap@bar.com');
+
+      assert.equal(identity.externalId, 'f123ldap');
+      assert.equal(identity.provider, 'ldap');
+      assert.deepEqual(identity.credentials, {
+        accessToken: 'atldap1',
+        refreshToken: 'rtldap1',
+      });
+
+      assert.equal(user.id, identity.userId);
+      assert(!token);
+
+      // Follow the belongsTo relation
+      identity.user(function(err, user) {
         if (err) return done(err);
 
         assert.equal(user.username, 'ldap.xyzldap');
         assert.equal(user.email, 'fooldap@bar.com');
 
-        assert.equal(identity.externalId, 'f123ldap');
-        assert.equal(identity.provider, 'ldap');
-        assert.deepEqual(identity.credentials, {accessToken: 'atldap1',
-          refreshToken: 'rtldap1'});
-
-        assert.equal(user.id, identity.userId);
-        assert(!token);
-
-        // Follow the belongsTo relation
-        identity.user(function(err, user) {
-          if (err) return done(err);
-
-          assert.equal(user.username, 'ldap.xyzldap');
-          assert.equal(user.email, 'fooldap@bar.com');
-
-          done();
-        });
+        done();
       });
+    });
   });
 
   it('supports accessToken option before email verification', function(done) {
-    User.create({
-      username: 'myLocalTest',
-      email: 'myLocalTest@local.com',
-      password: 'pass',
-      emailVerified: 0,
-    }, function(err, user) {
-      if (err) return done(err);
-      assert.equal(user.username, 'myLocalTest');
-      assert.equal(user.email, 'myLocalTest@local.com');
-      assert.equal(user.emailVerified, false);
-      User.login({username: user.username, password: 'notpass'},
-        function(err, user) {
+    User.create(
+      {
+        username: 'myLocalTest',
+        email: 'myLocalTest@local.com',
+        password: 'pass',
+        emailVerified: 0,
+      },
+      function(err, user) {
+        if (err) return done(err);
+        assert.equal(user.username, 'myLocalTest');
+        assert.equal(user.email, 'myLocalTest@local.com');
+        assert.equal(user.emailVerified, false);
+        User.login({username: user.username, password: 'notpass'}, function(err, user) {
           assert(err, 'LOGIN_FAILED');
         });
-      User.login({username: user.username, password: 'pass'},
-        function(err, user) {
+        User.login({username: user.username, password: 'pass'}, function(err, user) {
           assert(err, 'LOGIN_FAILED');
         });
-      done();
-    });
+        done();
+      }
+    );
   });
 
   it('supports accessToken option after verification', function(done) {
-    User.create({
-      username: 'myLocalTest2',
-      email: 'myLocalTest2@local.com',
-      password: 'pass',
-      emailVerified: 1,
-    }, function(err, user) {
-      if (err) return done(err);
-      assert.equal(user.username, 'myLocalTest2');
-      assert.equal(user.email, 'myLocalTest2@local.com');
-      assert.equal(user.emailVerified, true);
-      User.login({username: user.username, password: 'notpass'},
-        function(err, user) {
+    User.create(
+      {
+        username: 'myLocalTest2',
+        email: 'myLocalTest2@local.com',
+        password: 'pass',
+        emailVerified: 1,
+      },
+      function(err, user) {
+        if (err) return done(err);
+        assert.equal(user.username, 'myLocalTest2');
+        assert.equal(user.email, 'myLocalTest2@local.com');
+        assert.equal(user.emailVerified, true);
+        User.login({username: user.username, password: 'notpass'}, function(err, user) {
           assert(err, 'LOGIN_FAILED');
         });
-      User.login({username: user.username, password: 'pass'},
-        function(err, user) {
+        User.login({username: user.username, password: 'pass'}, function(err, user) {
           if (err) return done(err);
         });
-      done();
-    });
+        done();
+      }
+    );
   });
 
   describe('emailOptional set to false', function(err) {
     it('creates a user when given an email address', function(done) {
       var username = 'fbu123';
-      UserIdentity.login('facebook', 'oAuth 2.0', {
-        email: 'abc123@example.com',
-        id: 'fbi123',
-        username: username,
-      }, {
-        accessToken: 'at1',
-        refreshToken: 'rt1',
-      }, {
-        emailOptional: false,
-        profileToUser: customProfileToUser,
-      }, function(err, user, identity, token) {
-        if (err) return done(err);
-        assert.equal(user.username, username);
-        done();
-      });
+      UserIdentity.login(
+        'facebook',
+        'oAuth 2.0',
+        {
+          email: 'abc123@example.com',
+          id: 'fbi123',
+          username: username,
+        },
+        {
+          accessToken: 'at1',
+          refreshToken: 'rt1',
+        },
+        {
+          emailOptional: false,
+          profileToUser: customProfileToUser,
+        },
+        function(err, user, identity, token) {
+          if (err) return done(err);
+          assert.equal(user.username, username);
+          done();
+        }
+      );
     });
 
     it('does not create a user if an email address was not given', function(done) {
       User.count(function(err, countBefore) {
         if (err) return done(err);
-        UserIdentity.login('facebook', 'oAuth 2.0', {
-          id: 'fbi234',
-          username: 'fbu234',
-        }, {
-          accessToken: 'at2',
-          refreshToken: 'rt2',
-        }, {
-          emailOptional: false,
-          profileToUser: customProfileToUser,
-        }, function(err, user, identity, token) {
-          assert(err === g.f('email is missing from the user profile'), 'Should report error');
-          assert(typeof user === 'undefined', 'Should not return a user instance');
-          User.count(function(err, countAfter) {
-            if (err) return done(err);
-            assert.equal(countBefore, countAfter,
-              'Expected user count after execution to remain the same');
-            done();
-          });
-        });
+        UserIdentity.login(
+          'facebook',
+          'oAuth 2.0',
+          {
+            id: 'fbi234',
+            username: 'fbu234',
+          },
+          {
+            accessToken: 'at2',
+            refreshToken: 'rt2',
+          },
+          {
+            emailOptional: false,
+            profileToUser: customProfileToUser,
+          },
+          function(err, user, identity, token) {
+            assert(err === g.f('email is missing from the user profile'), 'Should report error');
+            assert(typeof user === 'undefined', 'Should not return a user instance');
+            User.count(function(err, countAfter) {
+              if (err) return done(err);
+              assert.equal(
+                countBefore,
+                countAfter,
+                'Expected user count after execution to remain the same'
+              );
+              done();
+            });
+          }
+        );
       });
     });
 
